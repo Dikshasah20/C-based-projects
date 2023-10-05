dc1ea1291b9b4bbd30a308ac839bc8291b99bd88
Diksha Sah <146458559+Dikshasah20@users.noreply.github.com>
2023-10-05 20:57:28 +0530

    Number Guessing Game
    Task 1

---
+++ game.cpp
@@ -0,0 +1,37 @@
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                #include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+    
+    srand(time(0));
+
+    
+    int secretNumber = rand() % 100 + 1;
+
+    int guess;
+    int attempts = 0;
+
+    cout << "Welcome to the Number Guessing Game!" << endl;
+    cout << "I have selected a number between 1 and 100. Try to guess it." << endl;
+
+    do {
+        cout << "Enter your guess: ";
+        cin >> guess;
+
+        attempts++;
+
+        if (guess < secretNumber) {
+            cout << "Too low! Try again." << endl;
+        } else if (guess > secretNumber) {
+            cout << "Too high! Try again." << endl;
+        } else {
+            cout << "Congratulations! You guessed the number in " << attempts << " attempts." << endl;
+        }
+    } while (guess != secretNumber);
+
+    return 0;
+}
+                                                                      
